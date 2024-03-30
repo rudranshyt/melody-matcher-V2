23b02ebffaf6aee862a10a628f4fde6dd1ae84f4
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2024-03-30 18:49:19 +0530

    added get playlist component and email service class

---
+++ force-app/main/default/lwc/getPlaylist/getPlaylist.js
@@ -0,0 +1,66 @@
+import { LightningElement, wire } from 'lwc';
+import getCriteriaValues from '@salesforce/apex/getCustomObjectPicklistValues.getCriteriaValues';
+import addRecordMethod from '@salesforce/apex/createRecord.addRecordMethod';
+import sendEmail from '@salesforce/apex/emailService.sendEmail';
+
+export default class GetPlaylist extends LightningElement {
+    criteriaValues = [];
+    name;
+    email;
+    criteria;
+
+    @wire(getCriteriaValues)
+    wiredGetCriteriaValues({ error, data }) {
+        if (data) {
+            this.criteriaValues = data;
+            console.log(this.criteriaValues);
+        }
+        if (error) {
+            const err = error
+            console.error('error---->', err);
+        }
+    }
+
+    handleName(event) {
+        this.name = event.target.value;
+    }
+    handleEmail(event) {
+        this.email = event.target.value;
+    }
+    handleCriteria(event) {
+        this.criteria = event.target.value;
+    }
+    handleSaveButton() {
+        addRecordMethod({
+            name: this.name,
+            email: this.email,
+            criteria: this.criteria
+        })
+            .then(() => {
+                sendEmail({
+                    emailId: this.email,
+                    criteria: this.criteria,
+                    name: this.name
+                }).then(() => {
+                    alert("Record created successfully, check email");
+                    this.name = '';
+                    this.email = '';
+                    this.criteria = '';
+                }).catch((error) => {
+                    console.log('error',error);
+                    alert('Email could not be sent, we are sorry for incovenience');
+                    this.name = '';
+                    this.email = '';
+                    this.criteria = '';
+                })
+
+            })
+            .catch(error => {
+                console.error('error--->', error);
+                alert("Record cannot be saved. Please try again later.");
+                this.name = '';
+                this.email = '';
+                this.criteria = '';
+            });
+    }
+}
--- force-app/main/default/lwc/homePageTab/homePageTab.js
+++ force-app/main/default/lwc/homePageTab/homePageTab.js
@@ -1,11 +1,12 @@
 import { LightningElement } from 'lwc';
+import { NavigationMixin } from "lightning/navigation";
 
-export default class HomePageTab extends LightningElement {
+export default class HomePageTab extends NavigationMixin(LightningElement) {
     handleClickButton() {
         const pageReference = {
             type: "standard__webPage",
             attributes: {
-                url: "/add-record"
+                url: "/get-playlist"
             }
         };
         this[NavigationMixin.Navigate](pageReference);
